54a54cb343722e9f3d66996c73865da77cdf2e37
Ken Scott <kenscott@gmail.com>
2025-07-27 18:23:03 -0500

    Fix malformed SHA256 hashes in cask file
    - Removed extra text from SHA256 fields that was causing brew upgrade errors
    - ARM64: aeaf923e6028b36fcad00462d872727f1528b7041abb910ca59dd939295289d3
    - Intel: 7d01ec5f562901a9cc4824db7ce5284a8a034a1ed8af4659cac50fa2ede2d1c5

--- Casks/storcat.rb
+++ Casks/storcat.rb
@@ -2,12 +2,10 @@ cask "storcat" do
   version "1.1.2"
   
   if Hardware::CPU.arm?
-    sha256 "📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2-arm64.dmg
-aeaf923e6028b36fcad00462d872727f1528b7041abb910ca59dd939295289d3"
+    sha256 "aeaf923e6028b36fcad00462d872727f1528b7041abb910ca59dd939295289d3"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}-arm64.dmg"
   else
-    sha256 "📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2.dmg
-7d01ec5f562901a9cc4824db7ce5284a8a034a1ed8af4659cac50fa2ede2d1c5"
+    sha256 "7d01ec5f562901a9cc4824db7ce5284a8a034a1ed8af4659cac50fa2ede2d1c5"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}.dmg"
   end
   
