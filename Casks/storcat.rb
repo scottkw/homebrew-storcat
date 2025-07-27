d9b3e2b654a10163582cd47b1229fd8342a2f902
Ken Scott <kenscott@gmail.com>
2025-07-27 18:19:15 -0500

    Update StorCat to version 1.1.2
    - Updated to StorCat v1.1.2
    - Updated SHA256 hashes for both architectures
    - ARM64: 📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2-arm64.dmg
    aeaf923e6028b36fcad00462d872727f1528b7041abb910ca59dd939295289d3
    - Intel: 📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2.dmg
    7d01ec5f562901a9cc4824db7ce5284a8a034a1ed8af4659cac50fa2ede2d1c5

--- Casks/storcat.rb
+++ Casks/storcat.rb
@@ -1,13 +1,16 @@
 cask "storcat" do
-  version "1.1.1"
+  version "1.1.2"
   
   if Hardware::CPU.arm?
-    sha256 "fcb513a4f1a58c6681f786ea894341b247524315eeb4ccd00e6ee6f5e4dd192a"
+    sha256 "📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2-arm64.dmg
+aeaf923e6028b36fcad00462d872727f1528b7041abb910ca59dd939295289d3"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}-arm64.dmg"
   else
-    sha256 "b378023fdec1c7c04fd0a5cba4c781741ac07f1600c00e3e1dbd55ee7b0c7d98"
+    sha256 "📥 Downloading: https://github.com/scottkw/storcat/releases/download/1.1.2/StorCat-1.1.2.dmg
+7d01ec5f562901a9cc4824db7ce5284a8a034a1ed8af4659cac50fa2ede2d1c5"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}.dmg"
   end
+  
   name "StorCat"
   desc "Directory Catalog Manager - Create, search, and browse directory catalogs"
   homepage "https://github.com/scottkw/storcat"
