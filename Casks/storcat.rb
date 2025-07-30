4bfe66176fb8326200ba072b528db984e4c86e14
Ken Scott <kenscott@gmail.com>
2025-07-30 13:05:12 -0500

    Update StorCat to version 1.2.2
    - Updated to StorCat v1.2.2
    - Updated SHA256 hashes for both architectures
    - ARM64: 1995ffb10bfa09887871f02f1e38b107381ca1f05d13adffb511b3eafbf1fc15
    - Intel: 0227c3fecb153eec4ac2f3eb32f15a75ce9ea4841ab30971ff5d75bc0f392193

--- Casks/storcat.rb
+++ Casks/storcat.rb
@@ -1,11 +1,11 @@
 cask "storcat" do
-  version "1.2.1"
+  version "1.2.2"
   
   if Hardware::CPU.arm?
-    sha256 "c1ebc127148f7a7c34000f1836552bb3c2da4eefd549f941a2982077c612698e"
+    sha256 "1995ffb10bfa09887871f02f1e38b107381ca1f05d13adffb511b3eafbf1fc15"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}-arm64.dmg"
   else
-    sha256 "2fef0ba51696dfc570fd5c8520d9b99c3c7b3ce340e8527ce9c3cc60cb62af69"
+    sha256 "0227c3fecb153eec4ac2f3eb32f15a75ce9ea4841ab30971ff5d75bc0f392193"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}.dmg"
   end
   
