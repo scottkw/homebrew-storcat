95219480628cab28087455e6301225d8a607e323
Ken Scott <kenscott@gmail.com>
2025-07-30 13:20:25 -0500

    Updated formula with new SHA256 hash values.

--- Casks/storcat.rb
+++ Casks/storcat.rb
@@ -1,25 +1,25 @@
 cask "storcat" do
   version "1.2.2"
-  
+
   if Hardware::CPU.arm?
-    sha256 "1995ffb10bfa09887871f02f1e38b107381ca1f05d13adffb511b3eafbf1fc15"
+    sha256 "0f21e51b9d1b3bed79396fd893526cb2d537e36df386c6f3b649cb0fa0f46976"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}-arm64.dmg"
   else
-    sha256 "0227c3fecb153eec4ac2f3eb32f15a75ce9ea4841ab30971ff5d75bc0f392193"
+    sha256 "8e98bd502463390710632c28b13544bff3ce612749cbf30515f193855fd5a144"
     url "https://github.com/scottkw/storcat/releases/download/#{version}/StorCat-#{version}.dmg"
   end
-  
+
   name "StorCat"
   desc "Directory Catalog Manager - Create, search, and browse directory catalogs"
   homepage "https://github.com/scottkw/storcat"
-  
+
   livecheck do
     url :url
     strategy :github_latest
   end
-  
+
   app "StorCat.app"
-  
+
   zap trash: [
     "~/Library/Application Support/StorCat",
     "~/Library/Preferences/com.kenscott.storcat.plist",
